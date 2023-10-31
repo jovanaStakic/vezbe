d6992c6914b0d28c85fa20a5432cfafca6fad43d
jovanaStakic <jovana.stakic997@gmail.com>
2023-10-31 16:43:15 +0100

    Dodato combo box

--- src/form/PrikazKnjigaForm.java
+++ src/form/PrikazKnjigaForm.java
@@ -6,6 +6,7 @@ package form;
 
 import domen.Knjiga;
 import java.util.List;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.table.TableModel;
 import model.KnjigaTableModel;
 import util.Biblioteka;
@@ -36,6 +37,7 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblKnjige = new javax.swing.JTable();
         DodajKnjigubtn = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,6 +61,13 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
             }
         });
 
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -68,7 +77,9 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 743, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(19, 19, 19)
+                .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(DodajKnjigubtn, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(15, 15, 15))
         );
@@ -78,7 +89,9 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
-                .addComponent(DodajKnjigubtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(DodajKnjigubtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(14, 14, 14))
         );
 
@@ -89,6 +102,10 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
         KnjigaTableModel model=(KnjigaTableModel) tblKnjige.getModel();
         model.dodajKnjigu();
     }//GEN-LAST:event_DodajKnjigubtnActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -127,6 +144,7 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton DodajKnjigubtn;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblKnjige;
     // End of variables declaration//GEN-END:variables
@@ -135,5 +153,6 @@ public class PrikazKnjigaForm extends javax.swing.JFrame {
         List<Knjiga> knjige=Biblioteka.vratiKnjige();
         TableModel model=new KnjigaTableModel(knjige);
         tblKnjige.setModel(model);
+        jComboBox1.setModel(new DefaultComboBoxModel<>(knjige.toArray()));
     }
 }
